380e6cd53d5cc1ac54f1c2c2f95aa4eca1096290
WanjohiWanjohi <wamuyuwanjohi97@gmail.com>
2022-07-26 17:13:40 +0300

    Load in currencies

--- src/App.js
+++ src/App.js
@@ -3,18 +3,21 @@ import './App.css';
 import ExchangeForm from './ExchangeForm';
 import HistoricalExchanges from './HistoricalExhanges';
 import React , {useState, useEffect} from 'react';
-import currencies from './../db.json';
 
 function App() {
+  const [currencies, setCurrencies] = useState([])
    // TODO: Load in these currencies from db.json as options to the select from and select to
 
   useEffect(() => { 
+    fetch("http://localhost:4000/currencies/")
+    .then((res)=> res.json()).then((data) =>console.log(data))
+    // .then((data) =>setCurrencies[data])
 
-    
+
   }, []);
   return (
     <div className="App">
-     <ExchangeForm/>
+     <ExchangeForm currencies={currencies}/>
      <HistoricalExchanges/>
     </div>
   );
