66268dba12afc9830af29977482196fd831e97c0
WanjohiWanjohi <wamuyuwanjohi97@gmail.com>
2022-07-26 15:41:01 +0300

    Add initial components

---
+++ src/ExchangeForm.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function ExchangeForm() {
+  return (
+    <div className="ExchangeForm">
+     
+    </div>
+  );
+}
+
+export default ExchangeForm;
---
+++ src/HistoricalExhanges.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function HistoricalExchanges() {
+  return (
+    <div className="ExchangeForm">
+     
+    </div>
+  );
+}
+
+export default HistoricalExchanges;
